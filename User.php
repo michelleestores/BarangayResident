f1f6aea216ba43157fa8b5126cf8d9975b633c20
michelleestores <130153845+michelleestores@users.noreply.github.com>
2023-04-07 20:19:32 +0800

    Create User.php

---
+++ User.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Barangay - Barangay Module (Resident's Information)
+ * 
+ * ESTORES, MICHELLE C. <michelle.estores@cvsu.edu.ph>
+ * 201810155
+ * 
+ * 2023-4-07
+ */
+
+namespace App\Controllers;
+
+
+class User extends BaseController
+{
+    public function username()
+    {
+        echo "";
+    }
+    public function password()
+    {
+        echo "";
+    }
+}
